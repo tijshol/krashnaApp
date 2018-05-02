a11e4c48322cb9ec2e77cb033b89a587a35ee8e4
tijshol <tijs.hol@gmail.com>
2018-05-02 17:21:23 +0200

    add home page with a few test features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule, ErrorHandler } from '@angular/core';
+import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
@@ -11,6 +12,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+
 @NgModule({
   declarations: [
     MyApp,
@@ -21,6 +23,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,59 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  eventList: any[];
+  notificationList: any[];
 
-  constructor(public navCtrl: NavController) {
+  constructor(private http: Http) {
+    // Fetch event list (local)
+  	let localData = http.get('../../assets/json/events-test.json').map(res => res.json().events);
+    localData.subscribe(data => {
+      this.eventList = data;
 
+      const now = new Date();
+      let previousItem = {};
+
+      for (let item of this.eventList) {
+        const timestamp = new Date(item.date + ' ' + item.time);
+        item.past = now > timestamp;
+        if (previousItem.past && !item.past)
+          item.highlight = true;
+        previousItem = item;
+      }
+
+      // Splice past events
+      for (let i = 0; i < this.eventList.length; i++) {
+        //if (this.eventList[i].past) this.eventList.splice(i,1); // Splice all
+        if (this.eventList[i].past && this.eventList[i+1].past) this.eventList.splice(i,1); // Leave the last
+      }
+    })
+
+    // Fetch notifications (currently also local)
+    let localData = http.get('../../assets/json/notifications-test.json').map(res => res.json().notifications);
+    localData.subscribe(data => {
+      this.notificationList = data;
+    })
   }
 
-}
+  openEvent(i) {
+    for (let j = 0; j < this.eventList.length; j++) {
+        this.eventList[j].open = i == j ? !this.eventList[j].open : 0;
+      }
+	}
+  deleteNotification(i) {
+    this.notificationList.splice(i,1);
+  }
+
+  doRefresh(refresher) {
+    setTimeout(() => {
+      refresher.complete();
+    }, 2000);
+  }
+
+  }
