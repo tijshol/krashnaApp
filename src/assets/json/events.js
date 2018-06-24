157bb3f79051adc61e27392f102db1159121e8a0
tijshol <tijs.hol@gmail.com>
2018-06-24 14:08:03 +0200

    update event list

--- src/assets/json/events.js
+++ src/assets/json/events.js
@@ -1,127 +1,177 @@
 export const events = [{
   id: 0,
-  name: "Busjourney to Bratislava",
+  name: "Departure to Bratislava",
   date: "2018/07/06",
-  time: "21:00"
+  time: "20:00",
+  location: "Unit Cultuur",
 }, {
   id: 1,
-  name: "Joint dinner",
+  name: "Arrival in Bratislava",
   date: "2018/07/07",
-  time: "19:00",
-  location: "Bratislava"
+  time: "10:30",
 }, {
   id: 2,
-  name: "Lucie game (Speurtocht)",
-  date: "2018/07/08",
-  time: "09:00",
-  location: "Bratislava"
+  name: "Joint dinner",
+  date: "2018/07/07",
+  time: "18:00",
+  location: "Bratislava (TBA)",
+  description: "Everybody eats together."
 }, {
   id: 3,
-  name: "Choir rehearsal",
-  date: "2018/07/08",
-  time: "16:00",
-  group: "Choir",
-  location: "Bratislava"
+  name: "Kraken borrel",
+  date: "2018/07/07",
+  time: "20:00",
+  location: "Kraken bar, Bratislava",
 }, {
   id: 4,
-  name: "Lucie dinner",
+  name: "Lucie game (Speurtocht)",
   date: "2018/07/08",
-  time: "20:00",
+  time: "10:00",
   location: "Bratislava"
 }, {
   id: 5,
+  name: "Lucie party",
+  date: "2018/07/08",
+  time: "20:00",
+  location: "Bratislava (TBA)",
+}, {
+  id: 6,
   name: "Choir rehearsal",
   date: "2018/07/09",
   time: "16:00",
-  timeEnd: "17:00",
+  timeEnd: "17:30",
   group: "Koor",
-  location: "Bratislava"
-}, {
-  id: 6,
-  name: "Orchestra Rehearsal",
-  date: "2018/07/09",
-  time: "17:00",
-  timeEnd: "19:00",
-  group: "Orkest",
-  location: "Bratislava"
+  location: "Slovak Radio Building, Bratislava",
 }, {
   id: 7,
+  name: "Orchestra rehearsal",
+  date: "2018/07/09",
+  time: "17:30",
+  timeEnd: "19:00",
+  group: "Orkest",
+  location: "Slovak Radio Building, Bratislava",
+}, {
+  id: 8,
   name: "Radio Building Concert",
   date: "2018/07/09",
   time: "20:00",
-  location: "Slovak Radio Building"
-}, {
-  id: 8,
-  name: "Busyourney to Vienna",
-  date: "2018/07/10",
-  time: "11:00",
+  location: "Slovak Radio Building, Bratislava",
 }, {
   id: 9,
-  name: "Schotten Kircke Concert",
-  date: "2018/07/10",
-  time: "20:00",
-  location: "Schotten Kircke"
+  name: "Bukowski borrel",
+  date: "2018/07/09",
+  time: "23:59",
+  location: "Bukowski bar, Bratislava",
 }, {
   id: 10,
-  name: "Party in Travelsack",
+  name: "Busjourney to Vienna",
   date: "2018/07/10",
-  time: "23:00",
-  location: "Travelsack"
+  time: "11:00",
+  timeEnd: "12:00",
+  location: "Hostel",
 }, {
   id: 11,
+  name: "KaMu Ensembles rehearsal",
+  date: "2018/07/10",
+  time: "14:00",
+  timeEnd: "17:30",
+  location: "Schottenkirche",
+}, {
+  id: 12,
+  name: "Rehearsal international ensembles",
+  date: "2018/07/10",
+  time: "17:00",
+  timeEnd: "19:30",
+  location: "TU Wien",
+}, {
+  id: 13,
+  name: "Schottenkirche Concert",
+  date: "2018/07/10",
+  time: "20:30",
+  location: "Schottenkirche",
+}, {
+  id: 14,
+  name: "After-Schottenkirche borrel",
+  date: "2018/07/10",
+  time: "23:00",
+  location: "Vienna (TBA)",
+}, {
+  id: 15,
   name: "Busjourney to Brno",
   date: "2018/07/11",
   time: "09:00",
+  timeEnd: "11:00",
+  location: "Hostel",
 }, {
-  id: 12,
+  id: 16,
+  name: "Rehearsal",
+  date: "2018/07/11",
+  time: "14:00",
+  location: "Square next to Church of St. Thomas"
+}, {
+  id: 17,
   name: "Brno Concert",
   date: "2018/07/11",
   time: "15:00",
   location: "Square next to Church of St. Thomas"
 }, {
-  id: 13,
+  id: 18,
   name: "Busjourney back to Vienna",
   date: "2018/07/11",
   time: "23:00",
+  timeEnd: "01:00",
+  location: "Square next to Church of St. Thomas"
 }, {
-  id: 14,
-  name: "Choir rehearsal",
+  id: 19,
+  name: "Rehearsal day",
   date: "2018/07/12",
-  time: "09:00",
-  timeEnd: "18:00",
-  group: "Koor",
-  location: "TU Wien"
+  time: "10:00",
+  group: "All",
+  location: "TU Wien",
+  description: "The starting time will change! Watch the notifications and listen to the Tourcie.",
 }, {
-  id: 15,
-  name: "Orchestra Rehearsal",
-  date: "2018/07/12",
-  time: "09:00",
-  timeEnd: "18:00",
-  group: "Orkest",
-  location: "TU Wien"
-}, {
-  id: 16,
+  id: 20,
   name: "Kuppel Saal Concert",
   date: "2018/07/12",
   time: "20:00",
   location: "Kuppel Saal, TU Wien"
 }, {
-  id: 17,
-  name: "Joint dinner and \"Bonte Middag\" ",
-  date: "2018/07/13",
-  time: "19:00",
-  timeEnd: "21:00",
-  location: "Vienna",
-  description: "Pizzas in the park"
+  id: 21,
+  name: "Party in Travelshack",
+  date: "2018/07/12",
+  time: "23:00",
+  location: "Travelshack, Vienna"
 }, {
-  id: 18,
-  name: "Busjournet back to Delft",
+  id: 22,
+  name: "Joint dinner and \"Bonte Avond\" (PIP)",
+  date: "2018/07/13",
+  time: "17:00",
+  timeEnd: "21:00",
+  location: "Donau Park, Vienna",
+  description: "PIP means Pizzas In the Park",
+}, {
+  id: 23,
+  name: "Departure back to Delft",
   date: "2018/07/13",
   time: "21:00",
+  location: "Donau Park, Vienna",
 }, {
-  id: 19,
+  id: 23,
+  name: "Arrival in Delft",
+  date: "2018/07/14",
+  time: "12:00",
+  location: "Unit Cultuur",
+  description: "Arrival time can vary.",
+}, {
+  id: 24,
+  name: "Rehearsal Amsterdam",
+  date: "2018/07/15",
+  time: "12:00",
+  location: "Muziekgebouw aan 't IJ, Amsterdam",
+}, {
+  id: 25,
   name: "Amsterdam Concert",
   date: "2018/07/15",
   time: "14:00",
-  location: "Muziekgebouw aan 't IJ, Amsterdam"
+  location: "Muziekgebouw aan 't IJ, Amsterdam",
 }, ];
