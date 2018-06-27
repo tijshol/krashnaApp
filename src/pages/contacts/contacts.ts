bf29cf218838ea1cc80203d78479a57fff8307f5
tijshol <tijs.hol@gmail.com>
2018-06-27 12:54:39 +0200

    change teams title and sort contacts

--- src/pages/contacts/contacts.ts
+++ src/pages/contacts/contacts.ts
@@ -10,13 +10,27 @@ import { contacts } from '../../assets/json/contacts.js';
   templateUrl: 'contacts.html'
 })
 export class ContactsPage {
-  contacts: object;
+  contacts: any;
+  sort: string;
 
   constructor(public navCtrl: NavController) {
   	this.contacts = contacts;
+  	this.sort = 'name';
+  	this.sortMembers();
   }
 
   goHome() {
     this.navCtrl.popToRoot();
+  }
+
+  sortMembers() {
+  	this.contacts.members.sort((a,b) => {
+  		if (a[this.sort] > b[this.sort])
+  			return 1;
+  		else if (a[this.sort] < b[this.sort])
+  			return -1;
+  		else
+  			return 0;
+  	});
   }
 }
