c1df386f8a6c5d9256c5374ce65c521fc045800b
tijshol <tijs.hol@gmail.com>
2018-06-30 00:00:46 +0200

    add a little joke

--- src/pages/teams/teams.ts
+++ src/pages/teams/teams.ts
@@ -6,11 +6,30 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'teams.html'
 })
 export class TeamsPage {
+  counter: number;
+  isTapping: boolean;
 
   constructor(public navCtrl: NavController) {
   }
 
   goHome() {
     this.navCtrl.popToRoot();
+  }
+
+  tapMusic() {
+  	console.log(this.counter);
+  	if (this.isTapping)
+  	  this.counter++;
+  	else {
+  	  this.counter = 0;
+  	  this.isTapping = true;
+  	  setTimeout(() => {
+  	  	this.isTapping = false;
+  	  	if (this.counter >= 10) {
+  	  	  window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+  	  	}
+  	  	this.counter = 0;
+  	  }, 2000);
+  	}
   }
 }
