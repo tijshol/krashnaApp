725a450f38e7827209dc7e461aa1c684332e689b
tijshol <tijs.hol@gmail.com>
2018-06-29 21:46:22 +0200

    add challenge checklist

--- src/pages/challenges/challenges.ts
+++ src/pages/challenges/challenges.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 import { challenges } from '../../assets/json/challenges.js';
 
@@ -9,12 +10,27 @@ import { challenges } from '../../assets/json/challenges.js';
 })
 export class ChallengesPage {
   challenges: any[];
+  checklist: any[];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public storage: Storage) {
   	this.challenges = challenges;
+  	this.checklist = [];
+  	storage.get('challenges').then(list => {
+      if (list)
+  		this.checklist = list;
+  	});
   }
 
   goHome() {
     this.navCtrl.popToRoot();
+  }
+
+  isChecked(id) {
+  	return this.checklist[id];
+  }
+
+  toggleCheck(id) {
+  	this.checklist[id] = !this.checklist[id];
+    this.storage.set('challenges', this.checklist);
   }
 }
